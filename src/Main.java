da00820aeeffca01c5b040aa988508a05c3faec3
user <user@DESKTOP-MLCAMPE>
2021-12-31 15:26:36 +0900

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("¿Â πŒ «ı");
+		System.out.println("Minhyeok Jang");
 
 	}
 
